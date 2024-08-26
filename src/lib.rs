68a91594c5a668f745e87679ed8bf03883496310
ian <me@iany.me>
2024-08-26 11:34:16 +0800

    setup the function prototype to create packet

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use secp256k1::{PublicKey, SecretKey};
+
+pub struct OnionPacket;
+pub struct Error;
+
+pub fn new_onion_packet(
+    _payment_path: Vec<PublicKey>,
+    _session_key: SecretKey,
+    _hops_data: Vec<Vec<u8>>,
+    _assoc_data: Vec<u8>,
+) -> Result<OnionPacket, Error> {
+    Ok(OnionPacket)
 }
 
 #[cfg(test)]
@@ -8,7 +18,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let packet = new_onion_packet(
+            vec![],
+            SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order"),
+            vec![],
+            vec![],
+        );
+        assert!(packet.is_ok());
     }
 }
